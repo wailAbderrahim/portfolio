c352945f047e33f5394fe6abc6c8ef7b21a7642b
wail abderrahim <abderrahimwail2@gmail.com>
2025-11-06 17:39:33 +0100

    removing data

--- data/data.js
+++ data/data.js
@@ -6,11 +6,5 @@ export const projects = [
         view: 'https://anfal-alquraan.vercel.app/',
         gitHub: 'https://github.com/wailAbderrahim/anfal-alquraan/tree/main/%D9%85%D9%88%D9%81%D8%B9%20%D9%82%D8%B1%D8%A7%D9%86%D9%8A%20v2'
     },
-        {
-        name: 'Accessory Manager App',
-        image: '/images/projects/accesoryManagerApp.png',
-        discription: 'A clean and intuitive product management web app that allows users to add, edit, and delete products efficiently.',
-        view: 'https://accessory-manager-r7pxfd7oq-abderrahimwails-projects.vercel.app/',
-        gitHub: 'https://github.com/wailAbderrahim/accessory-manager-app/tree/main/%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC%20%D8%A7%D8%AF%D8%A7%D8%B1%D8%A9%20%D9%85%D8%AD%D9%84%20%D8%A7%D9%83%D8%B3%D9%8A%D8%B3%D9%88%D8%A7%D8%B1%D8%A7%D8%AA%20%D8%A7%D9%84%D9%87%D8%A7%D8%AA%D9%81'
-    },
+    
 ];
