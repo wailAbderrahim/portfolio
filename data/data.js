2c4f150b3f133fa38a1bf057a0df492cf8930592
wail abderrahim <abderrahimwail2@gmail.com>
2025-11-07 21:52:15 +0100

    fixing dark mod

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         name: 'Quraan App',
-        image:'/images/projects/quraan-app.png',
+        image:'/images/projects/quraanApp.png',
         discription: ' A beautifully designed Quran web app offering the complete Quran experience with smooth navigation, responsive layout, and extra features for easy reading and listening..',
         view: 'https://anfal-alquraan.vercel.app/',
         gitHub: 'https://github.com/wailAbderrahim/anfal-alquraan/tree/main/%D9%85%D9%88%D9%81%D8%B9%20%D9%82%D8%B1%D8%A7%D9%86%D9%8A%20v2'
